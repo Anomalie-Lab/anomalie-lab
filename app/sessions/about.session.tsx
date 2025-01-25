2aa61788ec4cb8a3b838521044ddb3dadd2a1b67
Fernando Esdras da Silva <fernaando.esdras@gmail.com>
2025-01-25 14:51:36 -0300

    fix: player

--- app/sessions/about.session.tsx
+++ app/sessions/about.session.tsx
@@ -3,26 +3,14 @@ import { motion, useInView } from "framer-motion";
 import { ConfigProvider, Tabs } from "antd";
 import React, { useRef } from "react";
 
-const AnimatedTabContent: React.FC<{ children: React.ReactNode }> = ({
-  children,
-}) => {
+const AnimatedTabContent: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const ref = useRef(null);
   const isInView = useInView(ref);
 
   return (
-    <motion.div
-      ref={ref}
-      initial={{ opacity: 0 }}
-      animate={isInView ? { opacity: 1 } : { opacity: 0 }}
-      transition={{ duration: 0.8, delay: 0.2 }}
-      className="mx-auto mt-12"
-    >
-      <h3 className="text-4xl w-1/2 text-center mx-auto font-bold">
-        WHAT IS LOREM IPSUM WHAT.
-      </h3>
-      <div className="mx-20 mt-12 font-medium text-xl text-justify">
-        {children}
-      </div>
+    <motion.div ref={ref} initial={{ opacity: 0 }} animate={isInView ? { opacity: 1 } : { opacity: 0 }} transition={{ duration: 0.8, delay: 0.2 }} className="mx-auto">
+      <h3 className="text-4xl w-1/2 text-center mx-auto font-bold">WHAT IS LOREM IPSUM WHAT.</h3>
+      <div className="mx-20 mt-12 font-medium">{children}</div>
     </motion.div>
   );
 };
@@ -31,64 +19,28 @@ export function AboutSession() {
   const items = [
     {
       key: "1",
-      label: <h2 className="pb-2  text-lg">Interviews</h2>,
-      children: (
-        <AnimatedTabContent>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged.
-        </AnimatedTabContent>
-      ),
+      label: <h2 className="pb-2 text-base px-[60px]">Interviews</h2>,
+      children: <AnimatedTabContent>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</AnimatedTabContent>,
     },
     {
       key: "2",
-      label: <h2 className="pb-2  text-lg">Awards</h2>,
-      children: (
-        <AnimatedTabContent>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged.
-        </AnimatedTabContent>
-      ),
+      label: <h2 className="pb-2 text-base px-[60px]">Awards</h2>,
+      children: <AnimatedTabContent>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</AnimatedTabContent>,
     },
     {
       key: "3",
-      label: <h2 className="pb-2 text-lg">Social</h2>,
-      children: (
-        <AnimatedTabContent>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged.
-        </AnimatedTabContent>
-      ),
+      label: <h2 className="pb-2 text-base px-[60px]">Social</h2>,
+      children: <AnimatedTabContent>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</AnimatedTabContent>,
     },
     {
       key: "4",
-      label: <h2 className="pb-2 text-lg">Exibitions</h2>,
-      children: (
-        <AnimatedTabContent>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged.
-        </AnimatedTabContent>
-      ),
+      label: <h2 className="pb-2 text-base px-[60px]">Exibitions</h2>,
+      children: <AnimatedTabContent>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</AnimatedTabContent>,
     },
   ];
 
   return (
-    <section className="max-w-[750px] mx-auto p-4 flex">
+    <section className="max-w-[750px] mx-auto p-4">
       <ConfigProvider
         theme={{
           token: {
@@ -108,13 +60,7 @@ export function AboutSession() {
           },
         }}
       >
-        <Tabs
-          centered
-          animated
-          defaultActiveKey="1"
-          items={items}
-          className=""
-        />
+        <Tabs centered animated defaultActiveKey="1" items={items} />
       </ConfigProvider>
     </section>
   );
