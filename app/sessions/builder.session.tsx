35c264417edb7480f1164b8984bd4ccae5e4ea53
Fernando Esdras da Silva <fernaando.esdras@gmail.com>
2025-01-27 20:11:40 -0300

    feat: 3d preserve em builder section

--- app/sessions/builder.session.tsx
+++ app/sessions/builder.session.tsx
@@ -2,22 +2,36 @@
 import Link from "next/link";
 import { LiaLongArrowAltRightSolid } from "react-icons/lia";
 import { trans } from "../libs/i18n.lib";
-import { useRef } from "react";
-import { useInView } from "framer-motion";
-import { motion } from "framer-motion";
+import { useState } from "react";
+
 export function BuilderSession() {
-  const ref = useRef(null);
-  const isInView = useInView(ref);
+  const [style, setStyle] = useState({});
+
+  const handleMouseMove = (e: any) => {
+    const { offsetX, offsetY, target } = e.nativeEvent;
+    const width = target.offsetWidth;
+    const height = target.offsetHeight;
+    const x = (offsetX / width - 0.5) * 20;
+    const y = (offsetY / height - 0.5) * -20;
+
+    setStyle({
+      transform: `rotateX(${y}deg) rotateY(${x}deg)`,
+    });
+  };
+
+  const handleMouseLeave = () => {
+    setStyle({
+      transform: "rotateX(0deg) rotateY(0deg)",
+    });
+  };
   return (
-    <motion.section ref={ref} initial={{ opacity: 0, width: "30%" }} animate={isInView ? { opacity: 1, width: "100%" } : { opacity: 0, width: "30%" }} transition={{ duration: 1 }} className="mt-24  h-[600px] flex items-end">
-      <div className="p-20 bg-black shadow-xl text-white w-full rounded-xl flex justify-between items-end h-96 max-md:p-8 max-md:h-72">
-        <motion.h4 ref={ref} initial={{ opacity: 0 }} animate={isInView ? { opacity: 1 } : { opacity: 0 }} transition={{ duration: 1.5, delay: 1 }} className="text-4xl max-w-96 font-medium max-md:text-2xl">
-          {trans.t("take the first step towards your digital success.")}
-        </motion.h4>
+    <section className="mt-24  h-[600px] flex items-end hover-card" onMouseMove={handleMouseMove} onMouseLeave={handleMouseLeave} style={{ perspective: "1000px" }}>
+      <div className="p-20 bg-black shadow-xl duration-500 text-white w-full rounded-xl flex justify-between items-end h-96 max-md:p-8 max-md:h-72 hover-content p" style={{ ...style }}>
+        <h4 className="text-4xl max-w-96 font-medium max-md:text-2xl">{trans.t("take the first step towards your digital success.")}</h4>
         <Link href="#" className="p-5 text-center bg-white text-black rounded-md text-2xl">
           <LiaLongArrowAltRightSolid />
         </Link>
       </div>
-    </motion.section>
+    </section>
   );
 }
