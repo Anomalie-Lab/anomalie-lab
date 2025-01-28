16ed9c0331a59a3a6956fd75738f284a5410fc83
Fernando Esdras da Silva <fernaando.esdras@gmail.com>
2025-01-28 20:12:59 -0300

    fix: link builder

--- app/sessions/builder.session.tsx
+++ app/sessions/builder.session.tsx
@@ -31,7 +31,7 @@ export function BuilderSession() {
     <section id="contacts" className="mt-24  h-[600px] flex items-end hover-card group" onMouseMove={handleMouseMove} onMouseLeave={handleMouseLeave} style={{ perspective: "1000px" }}>
       <div className="p-20 bg-black shadow-xl duration-500 text-white w-full rounded-xl flex justify-between items-end h-96 max-md:p-8 max-md:h-72 hover-content p" style={{ ...style }}>
         <h4 className="text-4xl max-w-96 font-medium max-md:text-2xl">{trans.t("take the first step towards your digital success.")}</h4>
-        <Link href="#" className="p-5 text-center bg-white text-black rounded-md text-2xl  hover:scale-125">
+        <Link href="https://wa.link/qr8nbk" target="_blank" className="p-5 text-center bg-white text-black rounded-md text-2xl  hover:scale-125">
           <LiaLongArrowAltRightSolid className="group-hover:rotate-45 duration-300" />
         </Link>
       </div>
